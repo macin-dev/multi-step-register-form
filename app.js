1a043eefbdd17dc5434115db2576c56bec8c391d
macin-dev <ernesto.dev24@gmail.com>
2026-02-10 11:00:30 -0600

    feat: highlight the checkbox's parent container when focusable

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const fieldSets = document.querySelectorAll(".form__group");
+const checkboxElements = document.querySelectorAll('input[type="checkbox"]');
 
 let currentStep = 1;
 
@@ -25,6 +26,18 @@ function prevStep() {
 
   if (currentStep >= 1) showStep(currentStep);
 }
+
+// It highlights the checked state of the checkbox input
+// wether it is true(add style) or false(remove style)
+checkboxElements.forEach((el) => {
+  el.addEventListener("change", (e) => {
+    if (e.target.checked) {
+      el.parentElement.classList.add("option--selected");
+    } else {
+      el.parentElement.classList.remove("option--selected");
+    }
+  });
+});
 
 document.getElementById("multiStepForm").addEventListener("submit", (event) => {
   event.preventDefault();
